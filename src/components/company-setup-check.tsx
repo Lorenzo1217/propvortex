97c26a7c5823510d31c047375a57ea5f12768934
Lorenzo1217 <lorenzoabarbara@gmail.com>
2025-08-14 21:09:51 -0500

    Fix CompanySetupCheck ensureUserInDatabase missing argument

--- src/components/company-setup-check.tsx
+++ src/components/company-setup-check.tsx
@@ -1,8 +1,14 @@
 import { redirect } from 'next/navigation'
+import { currentUser } from '@clerk/nextjs/server'
 import { ensureUserInDatabase } from '@/lib/user-helpers'
 
 export async function CompanySetupCheck() {
-  const user = await ensureUserInDatabase()
+  const clerkUser = await currentUser()
+  if (!clerkUser) {
+    redirect('/login')
+  }
+  
+  const user = await ensureUserInDatabase(clerkUser)
   
   // If user doesn't have a company and is not on the setup page, redirect to setup
   if (!user.companyId) {
